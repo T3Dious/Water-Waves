d8fcf2bd9cb3e1223981c99d00f584316e1aefe1
T3Dious <104497364+T3Dious@users.noreply.github.com>
2024-12-09 04:06:44 +0200

    Update Waves.cs

--- Waves.cs
+++ Waves.cs
@@ -6,30 +6,30 @@ using System;
 
 public class Waves : MonoBehaviour
 {
-    MeshFilter meshFilter;
+    MeshFilter waterMeshFilter;
 
-    Mesh mesh;
+    Mesh waterMesh;
 
-    MeshRenderer meshRenderer;
+    MeshRenderer waterMeshRenderer;
 
     public WavesVariables Wvariable;
 
     public WavesAttribute waves;
 
-    List<Vector3> vertices;
+    List<Vector3> waterVertices;
 
-    List<int> triangles;
+    List<int> waterTriangles;
 
-    List<Vector2> uvs;
+    List<Vector2> waterUVS;
 
     public float frequency = 1, maxHeight = 16, minHeight = 1;
 
     private void OnValidate()
     {
-        mesh = new Mesh();
-        meshFilter = GetComponent<MeshFilter>();
-        meshFilter.mesh = mesh;
-        meshRenderer = GetComponent<MeshRenderer>();
+        waterMesh = new Mesh();
+        waterMeshFilter = GetComponent<MeshFilter>();
+        waterMeshFilter.mesh = waterMesh;
+        waterMeshRenderer = GetComponent<MeshRenderer>();
         GeneratingPlane();
     }
 
@@ -39,16 +39,17 @@ public class Waves : MonoBehaviour
         float zStep = Wvariable.Dimension.y / Wvariable.Resolution;
         float y = 0;
         y = FractalBrownianMotion(xStep, zStep);
-        // for (int i = 0; i < waves.waves.Count; i++) soon will find a way to set an array of vector4 to apply in shaders so that y can have max waves
-        // {
-        //     meshRenderer.material.SetVectorArray("_WaveA", waves.waves);
-        // }
+        for (int i = 0; i < waves.waves.Count; i++) //soon will find a way to set an array of vector4 to apply in shaders so that y can have max waves
+        {
+            waterMeshRenderer.material.SetInt("_NumberOfWaves", waves.waves.Count);
+            waterMeshRenderer.material.SetVectorArray("_Waves", waves.waves);
+        }
 
     }
 
     void GeneratingPlane()
     {
-        vertices = new List<Vector3>();
+        waterVertices = new List<Vector3>();
         float xStep = Wvariable.Dimension.x / Wvariable.Resolution;
         float zStep = Wvariable.Dimension.y / Wvariable.Resolution;
 
@@ -58,43 +59,43 @@ public class Waves : MonoBehaviour
             {
                 float y = 0;
                 y = FractalBrownianMotion(width, depth);
-                vertices.Add(new Vector3(depth * xStep, y, width * zStep));
+                waterVertices.Add(new Vector3(depth * xStep, y, width * zStep));
             }
         }
 
-        triangles = new List<int>();
+        waterTriangles = new List<int>();
         for (int r = 0; r < Wvariable.Resolution; r++)
         {
             for (int c = 0; c < Wvariable.Resolution; c++)
             {
                 int i = (r * Wvariable.Resolution) + r + c;
 
-                triangles.Add(i);
-                triangles.Add(i + (Wvariable.Resolution) + 1);
-                triangles.Add(i + (Wvariable.Resolution) + 2);
+                waterTriangles.Add(i);
+                waterTriangles.Add(i + (Wvariable.Resolution) + 1);
+                waterTriangles.Add(i + (Wvariable.Resolution) + 2);
 
-                triangles.Add(i);
-                triangles.Add(i + (Wvariable.Resolution) + 2);
-                triangles.Add(i + 1);
+                waterTriangles.Add(i);
+                waterTriangles.Add(i + (Wvariable.Resolution) + 2);
+                waterTriangles.Add(i + 1);
             }
         }
 
-        uvs = new List<Vector2>();
+        waterUVS = new List<Vector2>();
         for (int x = 0; x <= Wvariable.Resolution; x++)
         {
             for (int z = 0; z <= Wvariable.Resolution; z++)
             {
                 var vec = new Vector2((x / Wvariable.UVScale) % 2, (z / Wvariable.UVScale) % 2);
-                uvs.Add(new Vector2(vec.x <= 1 ? vec.x : 2 - vec.x, vec.y <= 1 ? vec.y : 2 - vec.y));
+                waterUVS.Add(new Vector2(vec.x <= 1 ? vec.x : 2 - vec.x, vec.y <= 1 ? vec.y : 2 - vec.y));
             }
         }
 
-        mesh.Clear();
-        mesh.vertices = vertices.ToArray();
-        mesh.triangles = triangles.ToArray();
-        mesh.uv = uvs.ToArray();
-        mesh.RecalculateNormals();
-        mesh.RecalculateBounds();
+        waterMesh.Clear();
+        waterMesh.vertices = waterVertices.ToArray();
+        waterMesh.triangles = waterTriangles.ToArray();
+        waterMesh.uv = waterUVS.ToArray();
+        waterMesh.RecalculateNormals();
+        waterMesh.RecalculateBounds();
     }
 
     //void FFT()
@@ -140,5 +141,7 @@ public class Waves : MonoBehaviour
     public struct WavesAttribute
     {
         public List<Vector4> waves;
+        // public List<float> directionX, directionY;
+        // public List<float> wavelength, steepness;
     }
 }
